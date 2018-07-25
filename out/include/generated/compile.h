ef39857850a660b3ab3884dbc7fbaa62abb2ac9e
Malpa73 <kmktoczek@gmail.com>
2018-07-25 14:13:09 +0200

    first commit

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 SMP PREEMPT Wed Jul 25 14:03:59 CEST 2018"
+#define LINUX_COMPILE_BY "malpa73"
+#define LINUX_COMPILE_HOST "malpa73-ThinkPad-T420"
+#define LINUX_COMPILER "gcc version 4.7.4 20140402 (prerelease) (Linaro GCC 4.7-2014.04-1~dev) "
